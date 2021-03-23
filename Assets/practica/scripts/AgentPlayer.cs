b309411854bd8411917961e4c681272633d98676
MonsignorEduardo <monsignor.eduardo@gmail.com>
2021-03-23 20:33:13 +0100

    AgentPlayer hecho y rigid body creado

---
+++ Assets/practica/scripts/AgentPlayer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgentPlayer : Agent
+{
+
+    void mover()
+    {
+        Vector3 Velocity = (new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"))) * this.vMaxima;
+        transform.Translate(Velocity * Time.deltaTime, Space.World);
+        transform.LookAt(transform.position + Velocity);
+        this.orientacion = transform.rotation.eulerAngles.y;
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mover();
+    }
+}
