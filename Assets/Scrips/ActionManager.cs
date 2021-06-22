e7c3000222844c1d582e623baa861bd7ec7b1b91
TheJavixuWe <50200270+Javixuwe@users.noreply.github.com>
2021-06-22 18:43:07 +0200

    Importado Behaviour Bricks

---
+++ Assets/Scrips/ActionManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+struct Action
+{
+    //expiryTime
+    //def CanInterrupt()
+    //def CanDoBoth(otherAction)
+    //def isComplete()
+}
+
+public class ActionManager : MonoBehaviour
+{
+    //Lista de acciones sin completar
+
+    //Lista de acciones en ejecución.
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
